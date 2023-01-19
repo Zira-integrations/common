40ca3fb8109816a70dc468737fa98244c84c8467
gil-zira <gil.cohen@zira.us>
2023-01-19 10:15:41 -0800

    formatting

--- src/readXls.ts
+++ src/readXls.ts
@@ -6,7 +6,11 @@ import { read, utils } from 'xlsx';
  * @param headerRow which row the headers are labeled (0-based index)
  * @param sheetNum optional, states which sheet should be read (0-based index)
  */
-function readXls (attachmentContent: Buffer, headerRow: number, sheetNum = 0): Array<any> {
+function readXls (
+  attachmentContent: Buffer,
+  headerRow: number,
+  sheetNum = 0
+): Array<any> {
   const file = read(attachmentContent);
   const data = utils.sheet_to_json(file.Sheets[file.SheetNames[sheetNum]], {
     raw: false,
