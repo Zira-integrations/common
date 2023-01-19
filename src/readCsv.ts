31256e1e2421eb79f9e1e8c4839a0da4d1783006
gil-zira <gil.cohen@zira.us>
2023-01-19 09:58:19 -0800

    Updated deps and formatting

--- src/readCsv.ts
+++ src/readCsv.ts
@@ -1,24 +1,23 @@
 import csv from 'fast-csv';
 const { Readable } = require('stream');
 
+function readCsv (csvBuffer: any, rowProcessor = (row: string) => row) {
+  return new Promise((resolve, reject) => {
+    const stream = Readable.from(csvBuffer);
 
+    const data: string[] = [];
 
-function readCsv(csvBuffer: any, rowProcessor = (row: string) => row) {
-    return new Promise((resolve, reject) => {
-        const stream = Readable.from(csvBuffer);
-
-        const data: string[] = [];
-
-        csv.parseStream(stream)
-            .on("error", reject)
-            .on("data", (row: string) => {
-                const obj = rowProcessor(row);
-                if (obj) data.push(obj);
-            })
-            .on("end", () => {
-                resolve(data);
-            });
-    });
+    csv
+      .parseStream(stream)
+      .on('error', reject)
+      .on('data', (row: string) => {
+        const obj = rowProcessor(row);
+        if (obj) data.push(obj);
+      })
+      .on('end', () => {
+        resolve(data);
+      });
+  });
 }
 
-export default readCsv
+export default readCsv;
