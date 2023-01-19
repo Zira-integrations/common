674a330527937adc18fec1d1bb1ed2c5ba37034e
gil-zira <gil.cohen@zira.us>
2023-01-19 10:35:52 -0800

    Changed API packages to be cached by default

--- src/getCollectionInfo.ts
+++ src/getCollectionInfo.ts
@@ -16,4 +16,28 @@ function getCollectionInfo (meterId: string, apiKey: string): Promise<any> {
     });
 }
 
-export default getCollectionInfo;
+function memoizePromiseFn (fn: Function): Function {
+  const cache = new Map();
+
+  return (...args: any) => {
+    const key = JSON.stringify(args);
+
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+
+    cache.set(
+      key,
+      fn(...args).catch((error: any) => {
+        // Delete cache entry if API call fails
+        cache.delete(key);
+        return Promise.reject(error);
+      })
+    );
+
+    return cache.get(key);
+  };
+}
+
+const cachedGetCollectionInfo = memoizePromiseFn(getCollectionInfo);
+export default cachedGetCollectionInfo;
--- src/meterSearch.ts
+++ src/meterSearch.ts
@@ -22,4 +22,28 @@ function meterSearch (searchKey: string, apiKey: string): Promise<string> {
     });
 }
 
-export default meterSearch;
+function memoizePromiseFn (fn: Function): Function {
+  const cache = new Map();
+
+  return (...args: any) => {
+    const key = JSON.stringify(args);
+
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+
+    cache.set(
+      key,
+      fn(...args).catch((error: any) => {
+        // Delete cache entry if API call fails
+        cache.delete(key);
+        return Promise.reject(error);
+      })
+    );
+
+    return cache.get(key);
+  };
+}
+
+const cachedMeterSearch = memoizePromiseFn(meterSearch);
+export default cachedMeterSearch;
