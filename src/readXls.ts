deb0d09a47c153671a74c0d9416ddeabfb4bd71a
Gil Cohen <gil.cohen@zira.us>
2023-02-07 09:25:32 -0800

    option for no headers

--- src/readXls.ts
+++ src/readXls.ts
@@ -3,21 +3,27 @@ import { read, utils } from 'xlsx';
 /**
  *
  * @param attachmentContent parsedEmail.attachments[0].content from simpleParser
- * @param headerRow which row the headers are labeled (0-based index)
+ * @param headerRow which row the headers are labeled (0-based index) If no headerRow, will read as arrays instead of json
  * @param sheetNum optional, states which sheet should be read (0-based index)
  */
 function readXls (
   attachmentContent: Buffer,
-  headerRow: number,
+  headerRow?: number,
   sheetNum = 0
 ): Array<any> {
   const file = read(attachmentContent);
-  const data = utils.sheet_to_json(file.Sheets[file.SheetNames[sheetNum]], {
+  if (headerRow === undefined) {
+    return utils.sheet_to_json(file.Sheets[file.SheetNames[sheetNum]], {
+      raw: false,
+      blankrows: false,
+      header: 1
+    });
+  }
+  return utils.sheet_to_json(file.Sheets[file.SheetNames[sheetNum]], {
     raw: false,
     blankrows: false,
     range: headerRow
   });
-  return data;
 }
 
 export default readXls;
